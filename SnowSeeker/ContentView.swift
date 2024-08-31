dd94714e8de9351a27c1e60ce313dd0cd343760e
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-31 09:31:35 +0200

    feat: filter by search

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchTerm = ""
+    
+    let names = ["Marcus Aurelius", "Seneca", "Zeno of Citium", "Zeno of tarsus", "Epictetus", "Claudius Maximus"]
+    
+    var filteredNames: [String] {
+        if searchTerm.isEmpty {
+            return names
+        }
+        
+        return names.filter { $0.localizedStandardContains(searchTerm)}
+    }
+    
     var body: some View {
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-            
-            Circle()
-                .frame(width: 400, height: 300)
+        NavigationStack {
+            List(filteredNames, id: \.self) { stoic in
+                Text(stoic)
+            }
+                .searchable(text: $searchTerm, prompt: "Search for something")
+                .navigationTitle("Search")
         }
     }
 }
