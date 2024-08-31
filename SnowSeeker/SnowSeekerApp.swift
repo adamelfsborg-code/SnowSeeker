770b566250ecc69e24f596e54716e7dfda3e46a6
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-31 08:46:26 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Adam Elfsborg on 2024-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
