544030adb11935499dbf2cc8bca5ed789ce8a506
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-31 09:21:14 +0200

    feat: view that fits

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,28 +7,15 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Adam")
-            Text("Country: Sweden")
-            Text("Pets: No pets")
-        }
-        .font(.title)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    
     var body: some View {
-        if horizontalSizeClass == .compact {
-            VStack(content: UserView.init)
-        } else {
-            HStack(content: UserView.init)
+        ViewThatFits {
+            Rectangle()
+                .frame(width: 500, height: 200)
+            
+            Circle()
+                .frame(width: 400, height: 300)
         }
-        
-        
     }
 }
 
